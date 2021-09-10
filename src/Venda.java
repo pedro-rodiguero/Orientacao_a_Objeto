7ece0546226cebb1c99bd5a752dec44913d4bbe2
pemiinem <pedro.m.rodiguero@gmail.com>
2021-09-10 17:51:10 -0300

    Update das classes

---
+++ src/Venda.java
@@ -0,0 +1,29 @@
+public class Venda {
+    private int qntProduto;
+    private String id;
+    private Long valor;
+
+    public Venda( int qntProduto, String id, Long valor){
+
+        this.qntProduto = qntProduto;
+        this.id = id;
+        this.valor = valor;
+    }
+
+
+    public void setqntProduto(int qntProduto){
+        this.qntProduto = qntProduto;
+    }
+
+    public void setid(String id){
+        this.id = id;
+    }
+
+    public void setvalor(Long valor){
+        this.valor = valor;
+    }
+
+    public Long getValor(){
+        return valor;
+    }
+}
