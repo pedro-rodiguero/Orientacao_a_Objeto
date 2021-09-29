0d36a104b55e9df07b23799321b01c75c23ea027
pemiinem <pedro.m.rodiguero@gmail.com>
2021-09-29 11:51:57 -0300

    Criacao dos objetos pre definidos

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,26 @@ public class Main {
 			"aba reta", "aba torta", "viseira"};
 	private static final String[] cor = {"azul", "preto", "verde", "roxo", "branco",
 			"rosa","vermelho","laranja"};
-	private static final int[] qntEstoque = {1,2,3,4,5,
-			3,4,5};
+	private static final int[] qntEstoque = {1, 2, 3, 4, 5,
+			3, 4, 5};
 	private static final Long[] valor = {10L, 15L, 20L, 25L, 30L,
 			20L, 30L, 40L};
-	
-	private static final Boolean[] tipoProduto = {true, true, true, true, true,
+	private static final Boolean[] tipoProdutos = {true, true, true, true, true,
 			false, false, false};
 	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		ArrayList<Produto> produtos = new ArrayList<>();
 		
-		for(int i =0; i<8; i++) {
+		for(int i =0; i<8; i++){
 			Produto produto =  new Produto(){};
 			produto.modelo = modelos[i];
 			produto.cor = cor[i];
 			produto.qntEstoque = qntEstoque[i];
 			produto.valor = valor[i];
-			produto.tipoProduto = tipoProduto[i];
-			
-			
+			if(tipoProdutos[i])
+				produto.tipoProduto = "Oculos";
+			else
+				produto.tipoProduto = "Bone";
 		}
 	}
 }
-	
