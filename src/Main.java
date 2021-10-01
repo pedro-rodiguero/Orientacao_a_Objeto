06f7ef0dc2e7da5716e1816d33cfb7ad2b3e8476
pemiinem <pedro.m.rodiguero@gmail.com>
2021-10-01 16:58:25 -0300

    Update da Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import interfaceGrafica.TelaCliente;
+import interfaceGrafica.TelaMenu;
 
 public class Main {
 
 	public static void main(String[] args){
-		new TelaCliente();
+		try {
+			new TelaMenu();			//Funcao para entrar no menu
+		} catch (Exception e) {		//e nas outras telas
+			e.printStackTrace();
+		}
 	}
-
 }
