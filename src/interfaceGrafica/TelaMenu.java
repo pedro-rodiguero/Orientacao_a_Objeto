f0f4ef310f70276de656b0f3d2ec0174580e4d88
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2021-10-05 21:44:52 -0300

    Mudanca na interface grafica

--- src/interfaceGrafica/TelaMenu.java
+++ src/interfaceGrafica/TelaMenu.java
@@ -1,6 +1,6 @@
 package interfaceGrafica;
 
-import dados.Loja;
+
 import input.Input;
 
 import javax.swing.JInternalFrame;
@@ -23,7 +23,6 @@ public class TelaMenu implements ActionListener{
 	private static JLabel titulo = new JLabel("Menu principal");
 	private static JButton cliente = new JButton("Cliente");
 	private static JButton venda = new JButton("Venda");
-	private static JButton loja = new JButton("Loja");
 	private static JButton produto = new JButton("Produto");
 	
 	
@@ -31,20 +30,18 @@ public class TelaMenu implements ActionListener{
 		titulo.setFont(new Font("Broadway", Font.BOLD, 20));
 		titulo.setBounds(100, 20, 300, 30);
 		cliente.setFont(new Font("Cambria", Font.BOLD, 13));
-		cliente.setBounds(70, 70, 100, 30);
+		cliente.setBounds(140, 60, 100, 30);
 		venda.setFont(new Font("Cambria", Font.BOLD, 13));
-		venda.setBounds(70, 130, 100, 30);
+		venda.setBounds(140, 110, 100, 30);
 		produto.setFont(new Font("Cambria", Font.BOLD, 13));
-		produto.setBounds(200, 70, 100, 30);
-		loja.setFont(new Font("Cambria", Font.BOLD, 13));
-		loja.setBounds(200, 130, 100, 30);
+		produto.setBounds(140, 160, 100, 30);
+		
 
 		janela.setLayout(null);
 		
 		janela.add(titulo);
 		janela.add(cliente);
 		janela.add(venda);
-		janela.add(loja);
 		janela.add(produto);
 	
 		
@@ -58,7 +55,6 @@ public class TelaMenu implements ActionListener{
 		
 		cliente.addActionListener(menu);
 		venda.addActionListener(menu);
-		loja.addActionListener(menu);
 		produto.addActionListener(menu);
 		
 	}
@@ -71,9 +67,6 @@ public class TelaMenu implements ActionListener{
 		
 		if(src == venda)
 			new TelaVenda();
-		
-		if(src == loja)
-			new TelaLoja();
 		
 		if(src == produto)
 			new TelaProduto();
