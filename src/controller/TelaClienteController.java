e69f54d644a7095c58584428c1af9ee5bc8c4e04
pemiinem <pedro.m.rodiguero@gmail.com>
2021-10-07 23:20:30 -0300

    criacao do backend da aplicacao

---
+++ src/controller/TelaClienteController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+
+import model.Cliente;
+import model.DB;
+import view.TelaCliente;
+
+public class TelaClienteController {
+	private final TelaCliente view;
+
+	public TelaClienteController(TelaCliente view) {
+		this.view = view;
+	}
+
+	public void executarBotao(ActionEvent botao) {
+		JButton botaoApertado = (JButton) botao.getSource();
+
+		if (botaoApertado == this.view.getCadastro()) {
+			String valorNome = view.getValorNome().getText();
+			String valorCpf = view.getValorCpf().getText();
+			Cliente cliente = new Cliente();
+
+			cliente.setNomeCliente(valorNome);
+			cliente.setCpf(valorCpf);
+			cliente.setCep("15456");
+			cliente.setTelefone("15545454");
+
+			DB.getListaclientes().add(cliente);
+		}
+
+		System.out.println(DB.getListaclientes());
+	}
+}
