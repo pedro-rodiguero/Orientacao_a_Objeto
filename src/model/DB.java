11c47e7c68f3875d87ff8d0d8ee2fb31341d507e
pemiinem <pedro.m.rodiguero@gmail.com>
2021-10-08 01:21:41 -0300

    update de algumas classes

--- src/model/DB.java
+++ src/model/DB.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 public abstract class DB {
 
 	private static final ArrayList<Cliente> listaClientes = new ArrayList<>();
-
 	public static ArrayList<Cliente> getListaclientes() {
 		return listaClientes;
 	}
 
-	private DB() {
+	private static final ArrayList<Produto> listaProdutos = new ArrayList<>();
+	public static ArrayList<Produto> getListaProdutos(){
+		return listaProdutos;
 	}
+
+	private DB() {}
 
 }
